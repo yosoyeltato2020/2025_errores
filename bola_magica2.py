8c2a2b4c99a43bbf13468d5efad257789fd5aa11
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-06-11 18:28:20 +0200

    bola magica con except

--- bola_magica2.py
+++ bola_magica2.py
@@ -6,24 +6,26 @@ FILENAME = "frases.txt"
 def mostrar_respuesta(respuestas):
     print(random.choice(respuestas))
 
-def cargar_respuestas(respuestas):
+def cargar_respuestas(nombre_archivo):
     try:
-        manejador_de_fichero = open("frases.txt")
+        manejador_de_fichero = open(nombre_archivo)
     except FileNotFoundError:
         print("Error archivo no encontrado")
+        return []
     except FileExistsError:
-        print("Error archivo no exite")
+        print("Error archivo no existe")
+        return []
     except PermissionError:
         print("no tienes permisos para este archivo")
+        return []
     
     respuestas = []
     for linea in manejador_de_fichero:
         respuestas.append(linea)
-
     manejador_de_fichero.close()
-
     return respuestas
 
 if __name__ == "__main__": 
     respuestas = cargar_respuestas(FILENAME)
-    mostrar_respuesta(respuestas)
+    if respuestas:
+        mostrar_respuesta(respuestas)
